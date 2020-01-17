95258839b6edb1eee8bda7cfffc5796d36fdae33
Anand Singh <42609735+Anand-S23@users.noreply.github.com>
2020-01-17 12:34:36 -0500

    Create hello_world.c

---
+++ C/hello_world.c
@@ -0,0 +1,34 @@
+/*
+Objective:
+In this challenge, we will learn some basic concepts of C that will get you started with the language. You will need to use the same syntax to read input and write output in many C challenges.
+
+Task:
+This challenge requires you to print  on a single line, and then print the already provided input string to stdout.
+
+Output Format:
+Print  on the first line, and the string from the given input on the second line.
+
+Sample Input 0:
+Welcome to C programming.
+
+Sample Output 0:
+Hello, World!
+Welcome to C programming.
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    printf("Hello, World!\n");
+    printf("%s", s);
+    
+    return 0;
+}
