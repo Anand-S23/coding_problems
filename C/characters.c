5ebae7f21ef3f7eb0de1eff43485ab8bdf5ecf47
Anand Singh <42609735+Anand-S23@users.noreply.github.com>
2020-01-17 12:38:54 -0500

    Update characters.c

--- C/characters.c
+++ C/characters.c
@@ -1,13 +1,6 @@
 /*
 Objective:
 This challenge will help you to learn how to take a character, a string and a sentence as input in C.
-To take a single character  as input, you can use scanf("%c", &ch ); and printf("%c", ch) writes a character specified by the argument char to stdout
-char ch;
-scanf("%c", &ch);
-printf("%c", ch);
-This piece of code prints the character .
-You can take a string as input in C using scanf(“%s”, s). But, it accepts string only until it finds the first space.
-In order to take a line as input, you can use scanf("%[^\n]%*c", s); where  is defined as char s[MAX_LEN] where  is the maximum size of . Here, [] is the scanset character. ^\n stands for taking input until a newline isn't encountered. Then, with this %*c, it reads the newline character and here, the used * indicates that this newline character is discarded.
 
 Task:
 You have to print the character, , in the first line. Then print  in next line. In the last line print the sentence, .
